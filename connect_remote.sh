c5352ccfc7a668f9055d73586fb5732beecbac31
AndreGraca98 <andre.p.g@sapo.pt>
2022-02-03 17:10:26 +0000

    Terminals now opens in order on first screen

--- connect_remote.sh
+++ connect_remote.sh
@@ -9,15 +9,15 @@ fi
 echo "Connecting to $remote"
 
 
-# Open ssh terminal on remote
-gnome-terminal --geometry=140x39 --title="SSH" -- bash -c "ssh -Xt $remote; exec bash" 
+# Open htop on remote
+gnome-terminal --geometry=140x11-0+900 --title="Monitoring CPU" -- bash -c "ssh -Xt $remote htop;exec bash"
 
 # Open gpustat on remote
-gnome-terminal --geometry=140x3 --title="Monitoring GPU" -- bash -c "ssh -Xt $remote<<EOT
+gnome-terminal --geometry=140x3-0+670 --title="Monitoring GPU" -- bash -c "ssh -Xt $remote<<EOT
 conda activate i3d; 
 gpustat -ucFP --watch --color;
 EOT;exec bash"
 
-# Open htop on remote
-gnome-terminal --geometry=140x11 --title="Monitoring CPU" -- bash -c "ssh -Xt $remote htop;exec bash"
+# Open ssh terminal on remote
+gnome-terminal --geometry=140x37-0+0 --title="SSH" -- bash -c "ssh -Xt $remote; exec bash" 
 
