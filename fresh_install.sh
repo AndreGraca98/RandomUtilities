583085f107d2fa91931137ce425db160931a446c
AndreGraca98 <andre.p.g@sapo.pt>
2022-02-09 20:24:33 +0000

    Added htop, gpustat to fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt upgrade
 
-# Install Anaconda
-python anaconda_downloader.py
+# Install htop
+sudo apt-get install htop    # Usage: htop
 
 # Install gparted
 sudo apt install gparted
@@ -10,5 +10,10 @@ sudo apt install gparted
 # Install git
 sudo apt install git
 
-# Install 
+# Install Anaconda
+python anaconda_downloader.py
+
+# Install GpuStat
+pip install gpustat    # Usage: gpustat -ucFP --watch --color;
+
 
