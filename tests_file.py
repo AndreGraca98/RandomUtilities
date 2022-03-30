26ed4c363f8b71c6f1f65243c9d9013a39619000
AndreGraca98 <andre.p.g@sapo.pt>
2022-03-30 14:44:08 +0100

    Tests file

--- tests_file.py
+++ tests_file.py
@@ -1,4 +1,15 @@
-from tools import Email, get_credentials, email_notification
+from pathlib import Path
+from tools.email_class import Email
+from tools.encryption import create_credentials_csv, generate_key, get_all_credentials, get_credentials
+
 
 if __name__ == '__main__':
-    Email().send(attachments='tools/some_file.txt')
+    # generate_key()
+    # create_credentials_csv(["email@domain"], ["password"], force=False)
+    
+    # print( get_credentials('domain') )
+    # print( get_all_credentials() )
+    
+    Email(sender_email=input('sender email: '), sender_pwd=input('sender pwd: ')).send(attachments='tools/some_file.txt')
+    
+    pass
